38a3d359e982d396c6a3344c803934b0c72c17b9
La02 <la0shu5@qq.com>
2023-12-29 17:48:12 +0800

    Initial commit

---
+++ src/main/java/club/la0shu5/BlogApiApplication.java
@@ -0,0 +1,13 @@
+package club.la0shu5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/club/la0shu5/BlogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package club.la0shu5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
